457c1f7237f714c241c43e6221c9195cb7e8ff1d
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-06-25 20:17:48 +0200

    Fixed result. Change return by Trhow new Error

--- routers/login.router.js
+++ routers/login.router.js
@@ -4,11 +4,17 @@ const userController = require("../controllers/usersControllers");
 
 router.post("/", async (req, res) => {
     try {
+
         const emailCheck = req.body.email;
         const passwordCheck = req.body.password;
+
         let token = await loginController.validate( emailCheck, passwordCheck );
         let user = await userController.userEmail( emailCheck );
+
         res.status(200).json( {token, user})
+        {
+            throw new Error("Wrong user or password")
+        }
     } catch (error) {
         return res.status(500).json({
             message: error.message
