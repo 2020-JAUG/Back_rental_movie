9e7a04f3b00d44596fe68868407f50c309b66c81
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-02 03:35:00 +0200

    Remove lines

--- config/nodemailerConfig.js
+++ config/nodemailerConfig.js
@@ -16,11 +16,11 @@ module.exports.sendConfirmationEmail = (name, email, confirmationCode) => {
   transport.sendMail({
     from: user,
     to: email,
-    subject: "Movie Retro  Por favor, confirma tu cuenta de correo.",
-    html: `<h1>Correo de Activación de cuenta</h1>
-        <h2>Hola ${name}</h2>
-        <p>Gracias por registrarte en Movie Retro.
-        Por favor, confirma tu email haciendo click en el siguiente enlace.</p>
+    subject: "Movie Retro, por favor confirma tu cuenta de correo.",
+    html: `<h1>Activación de Cuenta</h1>
+        <h2>Hola ${name},</h2>
+        <p>Gracias por registrarte en Movie Retro,
+        por favor confirma tu email haciendo click en el siguiente enlace.</p>
         <a href=http://localhost:3001/users/confirm/${confirmationCode}> Activar cuenta.</a>
         </div>`,
   }).catch(err => console.log(err));
@@ -44,17 +44,3 @@ module.exports.sendConfirmationEmailNewClass = (name, email, roomName, roomDateS
         </div>`,
   }).catch(err => console.log(err));
 };
-
-// module.exports.sendReviewClass = (name, email) => {
-//   transport.sendMail({
-//     from: user,
-//     to: email,
-//     subject: "xSmile Fitness - Cuentanos tus impresiones.",
-//     html: `<h1>Queremos saber que te pareció la clase de hoy</h1>
-//         <h2>Hola ${name}</h2>
-//         Haz click para ver el formulario.
-//         <a href="https://docs.google.com/forms/d/e/1FAIpQLSf4Mkq0_Zs7Y2aKbpEABvEDBRyY0v1YEzuCNdoC2pqpyWVLIg/viewform?embedded=true" width="640" height="1286" frameborder="0" marginheight="0" marginwidth="0">Click</iframe>
-
-//         </div>`,
-//   }).catch(err => console.log(err));
-// };
