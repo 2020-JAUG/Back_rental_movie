a6c2613448ba918d513bdaf266210c6b9cc866a8
Alejandro Urbina <urbinagonzalezja@gmail.com>
2021-07-06 23:59:09 +0200

    Fixed to login result

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -8,53 +8,41 @@ class LoginController {
 
     async validate( emailCheck, passwordCheck ) {
 
-      let user = await userController.userEmail(emailCheck);
+        let user = await userController.userEmail(emailCheck);
 
-      if (user == null){
-        throw new Error('Wrong user or password');
-      }
+        if(user == null) {
+            var errorText = err.response.data.message;
+            console.log(errorText);
+            if (errorText.includes("email")){
+              setError(JSON.stringify("El email ya esta registrado."));
 
-      let password = user.password;
-
-      let verificar = await bcrypt.compare(passwordCheck,password);
-
-        if(!verificar){
-          throw new Error('Wrong user or password');
+            } else if (errorText.includes("phone")){
+              setError(JSON.stringify("El telefono ya esta registrado."));
+            }else{
+              setError(JSON.stringify(err.response.data.message));
+            }
+            return Error("Files not Found");
+            // throw new Error("Wrong user or password");
         }
+
+        let password = user.password;
+
+        let verificar = await bcrypt.compare(passwordCheck,password);
+
         if(!verificar) {
             throw new Error("Wrong user or password");
-        }
 
-        if (!user.isActive) {
-          throw new Error("The account is not active. Please, check your email and activate your account.");
-        }
+        } else {
 
-        let payload = {
-            userId: user.id,
-            createdAt: new Date,
-            isAdmin: user.isAdmin
+            let payload = {
+                userId: user.id,
+                createdAt: new Date,
+                isAdmin: user.isAdmin
+            };
+            return jwt.sign(payload, secret);
         }
-
-        return jwt.sign(payload, secret);
     }
-
 }
 
 let loginController = new LoginController();
 module.exports = loginController;
-
-        // if(user == null) {
-
-        //     var errorText = err.response.data.message;
-
-        //     if (errorText.includes("email")){
-        //       setError(JSON.stringify("El email ya esta registrado."));
-
-        //     } else if (errorText.includes("phone")){
-        //       setError(JSON.stringify("El telefono ya esta registrado."));
-        //     }else{
-        //       setError(JSON.stringify(err.response.data.message));
-        //     }
-        //     return Error("Files not Found");
-        //     // throw new Error("Wrong user or password");
-        // }
